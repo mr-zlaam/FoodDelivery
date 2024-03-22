ec5985d12b00614b404d9490f3a60f463e0d4ae3
Zlaam <mrzalaam@gmail.com>
2024-03-22 16:03:36 +0500

    created Login Form

--- src/app/_components/resturantLogin.tsx
+++ src/app/_components/resturantLogin.tsx
@@ -1,9 +1,42 @@
 import {} from "react";
+import Button from "../_reuseComp/Button/Button";
 
 function ResturantLogin() {
   return (
     <>
-      <section>ResturantLogin</section>
+      <section className="">
+        <form className="">
+          <div className="my-2">
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+              required
+              className="outline-none py-2 px-4 rounded-md font-semibold"
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="my-2">
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              required
+              className="outline-none py-2 px-4 rounded-md font-semibold"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter Password"
+            />
+          </div>
+          <div className="flex justify-end">
+            <Button variant="primary" className="">
+              Login
+            </Button>
+          </div>
+        </form>
+      </section>
     </>
   );
 }
--- src/app/resturant/page.tsx
+++ src/app/resturant/page.tsx
@@ -1,13 +1,44 @@
-import {} from "react";
+"use client";
+import { useState } from "react";
 import ResturantLogin from "../_components/resturantLogin";
 import ResturantSignUp from "../_components/resturantSignUp";
+import Link from "next/link";
 
 function ResturantPage() {
+  const [login, setLogin] = useState<boolean>(true);
   return (
     <>
-      <section>
-        <ResturantLogin />
-        <ResturantSignUp />
+      <section className="min-h-[70vh]  flex flex-col items-center justify-center">
+        <div className=" p-5 bg-blue-200">
+          <h1 className="text-center text-3xl font-bold my-3">
+            {login ? "Log in" : "Create an account"}
+          </h1>
+
+          {login ? <ResturantLogin /> : <ResturantSignUp />}
+          {login ? (
+            <p className="my-2">
+              Do not have an Account ?{" "}
+              <Link
+                className="duration-200 hover:opacity-70 hover:underline"
+                href="#"
+                onClick={() => setLogin((prev) => !prev)}
+              >
+                {login ? "Create One" : "Login"}
+              </Link>
+            </p>
+          ) : (
+            <p className="my-2">
+              Already Have an Account ?{" "}
+              <Link
+                className="duration-200 hover:opacity-70 hover:underline"
+                href="#"
+                onClick={() => setLogin((prev) => !prev)}
+              >
+                Login
+              </Link>
+            </p>
+          )}
+        </div>
       </section>
     </>
   );
